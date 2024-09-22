8e22504ecce56861766ec8ca877177e8d0039d64
user <user@localhost.localdomain>
2024-09-22 13:31:09 +0200

    fixed docker build for frontend

--- roadrecon/frontend-ng/vite.config.js
+++ roadrecon/frontend-ng/vite.config.js
@@ -28,7 +28,6 @@ export default defineConfig({
       '/api': {
         target: 'http://localhost:5000',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
       },
     }
   }
