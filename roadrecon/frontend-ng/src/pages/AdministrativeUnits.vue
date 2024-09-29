cf83ddbf67d480a8c940d9011cdeb0367225644d
user <user@debian>
2024-09-29 19:10:59 +0200

    Added administrativeunits page

--- roadrecon/frontend-ng/src/pages/AdministrativeUnits.vue
+++ roadrecon/frontend-ng/src/pages/AdministrativeUnits.vue
@@ -9,35 +9,58 @@
         <div class="mb-4 sm:mb-0">
           <h1 class="text-2xl md:text-3xl text-gray-800 dark:text-gray-100 font-bold">{{ name }}</h1>
         </div>
-
-        <!-- Right: Actions -->
-        <div class="grid grid-flow-col sm:auto-cols-max justify-start sm:justify-end gap-2">
-
-          <!-- Filter button -->
-          <FilterButton align="right" />
-          <!-- Datepicker built with flatpickr -->
-          <Datepicker align="right" />
-        </div>
-
       </div>
       <!-- Cards -->
-      <div class="grid grid-cols-12 gap-6">
+      <div class="grid gap-6">
+        <ObjectTable :columns="columns" :values="administrativeUnits" :filterFields="filterFields" :filters="filters" />
       </div>
     </div>
   </main>
 </template>
 
 <script>
-import { ref } from 'vue'
+import { ref, toRaw } from 'vue'
+import ObjectTable from '../partials/dashboard/ObjectTable.vue'
+import { FilterMatchMode } from '@primevue/core/api';
+import axios from 'axios'
+
+const filters = ref();
 
 export default {
   name: 'AdministrativeUnits',
-  components: {
-  },
   props: {
     name: String
   },
-  setup() {
+  components: {
+    ObjectTable
+  },
+  data(){
+    return {
+      administrativeUnits: [],
+      columns: [
+        { field: 'displayName', header: 'Name' },
+        { field: 'description', header: 'Description' },
+        { field: 'membershipRule', header: 'Membership rule' },
+      ],
+      filterFields:["displayName","description","membershipRule"],
+      filters: {
+        global: { value: null, matchMode: FilterMatchMode.CONTAINS },
+      },
+    }
+  },
+  mounted() {
+    axios
+        .get("/api/administrativeunits")
+        .then(response => {
+            const administrativeUnits = response.data
+
+            console.log("API data:", administrativeUnits)
+
+            this.administrativeUnits=response.data;
+        })
+        .catch(error => {
+            console.log(error)
+      })
   }
 }
 </script>
