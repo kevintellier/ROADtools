ced8ccc56a4c2706dada019742d808965fbbced4
kevintellier <kevincf.tellier@gmail.com>
2025-01-05 14:17:53 +0100

    Replaced logo with Roadtools

--- roadrecon/frontend-ng/src/partials/Sidebar.vue
+++ roadrecon/frontend-ng/src/partials/Sidebar.vue
@@ -28,9 +28,9 @@
         </button>
         <!-- Logo -->
         <router-link class="block" to="/">
-          <svg class="fill-violet-500" xmlns="http://www.w3.org/2000/svg" width="32" height="32">
-            <path d="M31.956 14.8C31.372 6.92 25.08.628 17.2.044V5.76a9.04 9.04 0 0 0 9.04 9.04h5.716ZM14.8 26.24v5.716C6.92 31.372.63 25.08.044 17.2H5.76a9.04 9.04 0 0 1 9.04 9.04Zm11.44-9.04h5.716c-.584 7.88-6.876 14.172-14.756 14.756V26.24a9.04 9.04 0 0 1 9.04-9.04ZM.044 14.8C.63 6.92 6.92.628 14.8.044V5.76a9.04 9.04 0 0 1-9.04 9.04H.044Z" />
-          </svg>
+          <div class="grid place-content-center">
+            <img width="60%" class="self-center m-auto" :src="Logo"/>
+          </div>
         </router-link>
       </div>
 
@@ -86,6 +86,7 @@
 import { ref, onMounted, onUnmounted, watch } from 'vue'
 import { useRouter } from 'vue-router'
 import { routes } from '../router'
+import Logo from '../images/logo.svg?url'
 
 import SidebarLinkGroup from './SidebarLinkGroup.vue'
 
@@ -96,7 +97,7 @@ export default {
     'variant'
   ],
   components: {
-    SidebarLinkGroup,
+    SidebarLinkGroup
   },
   data(){
 
@@ -105,7 +106,7 @@ export default {
     }
   },
   setup(props, { emit }) {
-
+    console.log(Logo)
     const items = ref(routes)
 
     const trigger = ref(null)
@@ -153,6 +154,7 @@ export default {
     })
 
     return {
+      Logo,
       trigger,
       sidebar,
       sidebarExpanded,
--- roadrecon/frontend-ng/vite.config.js
+++ roadrecon/frontend-ng/vite.config.js
@@ -1,13 +1,14 @@
 import path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import svgLoader from 'vite-svg-loader'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   define: {
     'process.env': process.env
   },
-  plugins: [vue()],
+  plugins: [vue(), svgLoader()],
   resolve: {
     alias: {
       '@tailwindConfig': path.resolve(__dirname, 'tailwind.config.js'),
